ccf17193fcad722fac5699aaf3df8157f52eae90
GregoryIwanek <iwanekgregory@gmail.com>
2016-05-08 00:51:21 +0200

    08.05.2016

--- algorythmnoun.cpp
+++ algorythmnoun.cpp
@@ -1,7 +1,104 @@
 #include "algorythmnoun.h"
+#include <stdlib.h>
 #include <QDebug>
 
-Noun::Noun()
+AlgorythmNoun::AlgorythmNoun()
 {
+setArticleSQLIndex();
+}
 
+void AlgorythmNoun::setData()
+{
+    defineVariables();
+    defineNounArticle();
+}
+
+void AlgorythmNoun::defineVariables()
+{
+    setIfIsSubject();
+    setGrammarCase();
+    setIfIsSingular();
+    setIfIsDefined();
+}
+
+void AlgorythmNoun::setIfIsSubject(bool isNounASubject)
+{
+    isSentenceSubject = isNounASubject;
+}
+
+void AlgorythmNoun::setNounSQLTable()
+{
+        nounTable = "NOUN";
+}
+
+void AlgorythmNoun::setGrammarCase()
+{
+    if (isSentenceSubject == true){
+        nounCase = "nominativ";
+    }
+    else nounCase = "dativ";
+}
+
+void AlgorythmNoun::setIfIsSingular()
+{
+        if (rand()%2 == 1){
+            isSingular = true;
+        }
+        else isSingular = false;
+}
+
+void AlgorythmNoun::setIfIsDefined()
+{
+        if (rand()%2 == 1){
+            isDefined = true;
+        }
+        else isDefined = false;
+}
+
+void AlgorythmNoun::defineNounArticle()
+{
+    setArticleIfSingular();
+    setArticleIfDefined();
+}
+
+void AlgorythmNoun::setArticleSQLTable()
+{
+    articleTable = "ARTICLE";
+}
+
+void AlgorythmNoun::setArticleIfSingular()
+{
+    int article;
+    article = rand()%3;
+
+    if (isSingular == false){
+        switch (article) {
+        case 0:
+            nounArticleColumn = "masculine_";
+            break;
+        case 1:
+            nounArticleColumn = "feminine_";
+            break;
+        case 2:
+            nounArticleColumn = "neutral_";
+            break;
+        }
+    }
+    else nounArticleColumn = "plurar_";
+}
+
+void AlgorythmNoun::setArticleIfDefined()
+{
+    if (isDefined == true){
+        nounArticleColumn.append("defined");
+    }
+    else nounArticleColumn.append("undefined");
+}
+
+void AlgorythmNoun::setArticleSQLIndex()
+{
+    if (nounCase == "nominativ") articleIndex = 0;
+    else if (nounCase == "genitiv") articleIndex = 1;
+    else if (nounCase == "dativ") articleIndex = 2;
+    else if (nounCase == "akkusativ") articleIndex = 3;
 }
--- algorythmnoun.h
+++ algorythmnoun.h
@@ -1,10 +1,30 @@
 #ifndef ALGORYTHMNOUN_H
 #define ALGORYTHMNOUN_H
+#include <QObject>
 
-class Noun{
+class AlgorythmNoun{
 public:
-    Noun();
+    AlgorythmNoun();
 
+    void setData();
+    void defineVariables();
+    void setIfIsSubject(bool isNounASubject = false);
+    void setNounSQLTable();
+    void setGrammarCase();
+    void setIfIsSingular();
+    void setIfIsDefined();
+    void defineNounArticle();
+    void setArticleSQLTable();
+    void setArticleIfSingular();
+    void setArticleIfDefined();
+    void setArticleSQLIndex();
+
+private:
+    QString querySQLArticle, querySQLNoun;
+    QString articleTable, nounTable;
+    QString nounCase, nounArticleColumn;
+    bool isSentenceSubject, isSingular, isDefined;
+    int articleIndex;
 };
 
 #endif // ALGORYTHMNOUN_H
